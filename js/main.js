8bac8a264d61d4cb2be8e5e917d50eb5c9966092
Bekhruz <117586415+Khayitaliev@users.noreply.github.com>
2025-09-19 10:13:37 +0500

    push

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
+// const burgerBtn = document.getElementById("burger-menu");
+// burgerBtn.onclick = function () {
+//     burgerBtn.classList.toggle("active")
+// }
+
+// const menu = document.querySelector(".nav");
+// const burgerBtn = document.querySelector(".burger-menu")
+// burgerBtn.addEventListener('click', function () {
+//     menu.classList.toggle("active__nav")
+// })
+
+
 const burgerBtn = document.getElementById("burger-menu");
+const nav = document.querySelector(".nav");
+
 burgerBtn.onclick = function () {
-    burgerBtn.classList.toggle("active")
-}
+    burgerBtn.classList.toggle("active");
+    nav.classList.toggle("active");
+};
